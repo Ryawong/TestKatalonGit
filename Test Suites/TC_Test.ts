80f28194e53ed8ab34ac10869d06c6addef6732a
mint <rattanapornya1@gmail.com>
2018-01-06 16:18:35 +0700

    Test

---
+++ Test Suites/TC_Test.ts
@@ -0,0 +1,25 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description></description>
+   <name>TC_Test</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>0</numberOfRerun>
+   <pageLoadTimeout>30</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <testSuiteGuid>26ff8448-c314-4ec4-abc4-91ac5f15b984</testSuiteGuid>
+   <testCaseLink>
+      <guid>2241568c-a153-49a3-948e-865ac95e5edf</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/TestCheckDuplicateRegister</testCaseId>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>349faf86-4cd9-43a8-9385-061edbe366c3</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/TestLoginVolunteer</testCaseId>
+   </testCaseLink>
+</TestSuiteEntity>
